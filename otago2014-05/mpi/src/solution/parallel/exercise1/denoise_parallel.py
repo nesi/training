322ea7df799270e111b50929dec29dc641497e09
Sung Bae <sung.bae@canterbury.ac.nz>
2014-04-26 21:40:23 +1200

    Revert "Added slides for MPI Python and updated source files"
    This reverts commit 1088a9a26e121c23495cf7aa0e1adb62b0b6781d.
    Should have gone to otago2014-05 branch

--- otago2014-05/mpi/src/solution/parallel/exercise1/denoise_parallel.py
+++ otago2014-05/mpi/src/solution/parallel/exercise1/denoise_parallel.py
@@ -1,4 +1,23 @@
 """
+=============================
+Denoising the picture of Lena
+=============================
+
+In this example, we denoise a noisy version of the picture of Lena using the
+total variation and bilateral denoising filter.
+
+These algorithms typically produce "posterized" images with flat domains
+separated by sharp edges. It is possible to change the degree of posterization
+by controlling the tradeoff between denoising and faithfulness to the original
+image.
+
+Total variation filter
+----------------------
+
+The result of this filter is an image that has a minimal total variation norm,
+while being as close to the initial image as possible. The total variation is
+the L1 norm of the gradient of the image.
+
 Bilateral filter
 ----------------
 
@@ -7,32 +26,48 @@ pixels based on their spatial closeness and radiometric similarity.
 
 """
 import numpy as np
-from skimage import data, img_as_float, filter, io
+
+from skimage import data, img_as_float
+from skimage.filter import denoise_tv_chambolle, denoise_bilateral,denoise_tv_bregman
+import skimage.io
+
 import os.path
 import time
 from mpi4py import MPI
+from numba import jit
 
 curPath = os.path.abspath(os.path.curdir)
-inputDir = os.path.join(curPath,'noisy')
-outputDirg = os.path.join(curPath,'denoised')
 
-def loop(imgFiles):
+noisyDir = os.path.join(curPath,'noisy')
+denoisedDir = os.path.join(curPath,'denoised')
+
+@jit
+def loop(imgFiles,rank):
 	for f in imgFiles:
-		img = img_as_float(data.load(os.path.join(inputDir,f))
+		img = img_as_float(data.load(os.path.join(noisyDir,f)))
 		startTime = time.time()
-		img = filter.denoise_bilateral(img, sigma_range=0.1, sigma_spatial=3)
-		io.imsave(os.path.join(outputDirg,f), img)
-		print("Took %f seconds for %s" %(time.time() - startTime, f))
+	#	img = denoise_tv_chambolle(img, weight=0.08, multichannel=True),
+		img = denoise_bilateral(img, sigma_range=0.1, sigma_spatial=3),
+		skimage.io.imsave(os.path.join(denoisedDir,f), img)
+		print("Process %d: Took %f seconds for %s" %(rank, time.time() - startTime, f))
 
 def parallel():
+
 	comm = MPI.COMM_WORLD
 	rank = comm.Get_rank()
 	size = comm.Get_size() #size must be 4!!
+	
 	totalStartTime = time.time()
 	
-	numFiles = 100/size #number of files this process will handle
-	imgFiles = ["%.4d.jpg"%x for x in range(rank*numFiles+1,(rank+1)*numFiles+1)] 
+	
+	imgFiles = os.listdir(noisyDir)
+	imgFiles=[x for x in imgFiles if '.jpg' in x]
+	imgFiles.sort()
+
+	numFiles = len(imgFiles)/size #number of files this process will handle
+	imgFiles = imgFiles[rank*numFiles:(rank+1)*numFiles] #list of files this process will handle
 	loop(imgFiles,rank)
+	
 	print("Process %d: Total time %f seconds" %(rank, time.time() - totalStartTime))
 
 if __name__=='__main__':
--- otago2014-05/mpi/src/solution/parallel/hello_p2p.py
+++ otago2014-05/mpi/src/solution/parallel/hello_p2p.py
@@ -4,7 +4,7 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if rank == 0:
-	for i in range(1,size):
+	for i in range(size):
 		sendMsg = "Hello, Rank %d" %i
 		comm.send(sendMsg, dest=i)
 else:
